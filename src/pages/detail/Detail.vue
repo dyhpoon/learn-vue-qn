71397d76a66b4dd9b8d7cdb8207d34e039f3373d
Darren Poon <dyhpoon@gmail.com>
2019-01-13 21:36:56 +0800

    Add detail list

--- src/pages/detail/Detail.vue
+++ src/pages/detail/Detail.vue
@@ -2,18 +2,48 @@
   <div>
     <detail-banner />
     <detail-header />
-    <div class="content"></div>
+    <div class="content">
+      <detail-list :list="list" />
+    </div>
   </div>
 </template>
 
 <script>
 import DetailBanner from './components/Banner'
 import DetailHeader from './components/Header'
+import DetailList from './components/List'
 export default {
   name: 'Detail',
   components: {
     DetailBanner,
-    DetailHeader
+    DetailHeader,
+    DetailList
+  },
+  data () {
+    return {
+      list: [
+        {
+          title: 'Adult',
+          children: [
+            {
+              title: 'Adult - Day Pass',
+            },
+            {
+              title: 'Adult - Two Day Pass'
+            }
+          ]
+        },
+        {
+          title: 'Student'
+        },
+        {
+          title: 'Child'
+        },
+        {
+          title: 'Senior'
+        }
+      ]
+    }
   }
 }
 </script>
